05dbe3d46705188e2e277539aebfe7a64f972cc8
son0nline <son0nline@yahoo.com.vn>
2017-07-05 15:47:32 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexCopy
+{
+    class Program
+    {
+        static string pattern = @".*FTKTE00538001\t\[SQL\]\t\[START\].*";
+        static void Main(string[] args)
+        {
+            List<string> rs = new List<string>();
+            string line;
+            Regex re = new Regex(pattern, RegexOptions.Compiled);
+
+            using(FileStream fs = new FileStream("a.txt", FileMode.Open, FileAccess.Read))
+            {
+                using(BufferedStream bs = new BufferedStream(fs))
+                {
+                    using(StreamReader sr = new StreamReader(bs, Encoding.GetEncoding("shift-jis")))
+                    {
+                        using(FileStream fsw = new FileStream("b.txt", FileMode.Append, FileAccess.Write))
+                        {
+                            using(StreamWriter sw = new StreamWriter(fsw, Encoding.GetEncoding("shift-jis")))
+                            {
+                                while((line = sr.ReadLine()) != null)
+                                {
+                                    if(re.IsMatch(line))
+                                    {
+                                        sw.WriteLine(line);
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+            Console.WriteLine("OK");
+            Console.ReadKey();
+        }
+    }
+}
